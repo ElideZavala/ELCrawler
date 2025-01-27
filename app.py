62026ea1b22fc6153753d2bd0aacce44304dc267
Elide <zavalavinagreelide@gmail.com>
2025-01-26 19:21:17 -0600

    Actualizar la selección de preguntas en el script de web scraping; agregar impresión de texto de preguntas

--- app.py
+++ app.py
@@ -8,6 +8,16 @@ response = requests.get(url)
 
 soup = BeautifulSoup(response.text, "html.parser")
 
-questions = soup.select(".question-summary")
-print(type(questions))
-print(questions[0].attrs)
+
+questions = soup.select(".s-post-summary")
+# print(type(questions))
+# print(questions)
+# print(questions[0].attrs)
+# print(questions[0].get("id", 0))
+# print(questions[0].select_one(".s-link"))
+# print(questions[0].select_one(".s-link").getText())
+
+# questions = soup.select(".s-link")
+for question in questions:
+    print(question.select_one(".s-link").getText())
+    print(question.select_one(".todo-no-class-here").getText())
